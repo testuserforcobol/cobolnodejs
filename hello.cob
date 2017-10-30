60f63a9b5d2c3dfcc53d437fdb27614814f32ad2
testuserforcobol <33194248+testuserforcobol@users.noreply.github.com>
2017-10-30 20:23:50 +0900

    Update hello.cob

--- hello.cob
+++ hello.cob
@@ -8,7 +8,7 @@
        MAIN SECTION.
            MOVE 0 TO CNT.
            PERFORM UNTIL CNT = 10
-               DISPLAY 'Hello World!'
+               DISPLAY 'HelloWorld!'
                COMPUTE CNT = CNT + 1
            END-PERFORM.
        EXIT-SECTION.
